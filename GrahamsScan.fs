ae44b1b1488bc768ce3c574a83ec3a479815b6e0
Blob2 <blobquadrat@gmail.com>
2017-11-30 01:30:33 +0100

    Started on Graham Scan

--- GrahamsScan.fs
+++ GrahamsScan.fs
@@ -1,15 +1,24 @@
 : whichSideLocals { a b c d e f }
 	a b c d e f e a - d b - * f b - c a - * - ;
 
-: graham (POINTS LENGTH - CONVEXHULL POINTS)
-	1 u+do
-		
+: graham ( POINTS LENGTH - CONVEXHULL POINTS )
+	dup 2 * 
+	6 u+do
+		swap >r
+	loop
 
-
+	3 u+do
+		begin
+			whichSideLocals .s 0 > while
+				drop drop
+		repeat
+		r> r>
+	loop ;
+		 
 : whichSide ( x1 y1 x2 y2 x y -- x1 y1 x2 y2 x y d )
 	swap >r >r 2swap 2dup r> swap - r> rot - ;
 
-: setup 
+test
 
-create x
-create y
+
+1 3 1 1 -1 -1 2 -3 3 0 5  .s graham .s
