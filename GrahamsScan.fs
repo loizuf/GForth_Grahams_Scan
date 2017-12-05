87610781afa401d9b3cceaf37e7cbbe5f126e2ff
Soeren <loizuf@gmx.de>
2017-12-05 12:49:31 +0100

    speichern funktioniert

--- GrahamsScan.fs
+++ GrahamsScan.fs
@@ -1,3 +1,4 @@
+\ determines wether Point (e,f) is to the left or to the right of the line between Point (a,b) and Point (c,d)
 : whichSideLocals { a b c d e f }
 	a b c d e f e a - d b - * f b - c a - * - ;
 
@@ -14,24 +15,30 @@
 		repeat
 		r> r>
 	loop ;
-		 
-: whichSide ( x1 y1 x2 y2 x y -- x1 y1 x2 y2 x y d )
-	swap >r >r 2swap 2dup r> swap - r> rot - ;
 
+\ solved with locals		 
+\ : whichSide ( x1 y1 x2 y2 x y -- x1 y1 x2 y2 x y d )
+\	swap >r >r 2swap 2dup r> swap - r> rot - ;
 
-: readLength (l -- )
-	create length , ;
-
-: readPoints (x1 y1 x2 y2 ... xn yn -- )
-	create points
-	length 0 u+do
-		,
-	loop
+\ saves all points in "points"
+: readPoints ( x1 y1 x2 y2 ... xn yn pointsadr length -- pointsadr length)
+	2 * dup
+	0 u+do
+		\ push the length on the return stack, arrange the next coordinate in the correct way (value adr) then take the length from the return stack, substract the loop counter and add that to the address. In this way we save the point in the correct order (x1 y1 x2 y2) 
+		>r swap over r> i i . over >r - cells + ! r>
+	loop ;
 
 0 Value fd-in
      0 Value fd-out
      : open-input ( addr u -- )  r/o open-file throw to fd-in ;
      : open-output ( addr u -- )  w/o create-file throw to fd-out ;
 
+: test
+	10 0 u+do
+		i .
+	loop ;
 
-1 3 1 1 -1 -1 2 -3 3 0 5  .s graham .s
+1 2 3 4 5 6 7 8 9 0 5
+create length , 
+create points length @ 2 * cells allot 
+points length @ readPoints
