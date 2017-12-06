0b5155b758e60361875ac876386b46148da3d726
Blob2 <blobquadrat@gmail.com>
2017-12-06 01:18:18 +0100

    Graham Scan is working, No sorting of points yet (input must be sorted by angle). Also no file is written yet

--- GrahamsScan.fs
+++ GrahamsScan.fs
@@ -2,43 +2,62 @@
 : whichSideLocals { a b c d e f }
 	a b c d e f e a - d b - * f b - c a - * - ;
 
-: graham ( POINTS LENGTH - CONVEXHULL POINTS )
-	dup 2 * 
-	6 u+do
-		swap >r
+: putFirstPoints ( pointsadr length - x1 y1 x2 y2 x3 y3 pointsadr length)
+	>r
+	6 0 u+do
+		dup i cells + @ swap
 	loop
+	r> swap ; ( take length from return stack, swap it behind the pointadr)
 
+: graham ( pointsadr length - CONVEXHULL POINTS )
+	
+	putFirstPoints
+
+	7 >r ( we create a counter which is pushed on the return stack, we count how many times we already skipped a vertex until we found the first three)
+	>r ( pointsadr pushed to returnstack)
+
+	\ iterate over the rest
 	3 u+do
-		begin
-			whichSideLocals .s 0 > while
-				drop drop
+		begin 
+			.s whichSideLocals 0 < while
+				2swap drop drop
 		repeat
-		r> r>
-	loop ;
 
-\ solved with locals		 
-\ : whichSide ( x1 y1 x2 y2 x y -- x1 y1 x2 y2 x y d )
-\	swap >r >r 2swap 2dup r> swap - r> rot - ;
+		r> r> r> r> ( take two loop parameters and pointadr and counter from return stack)
+		dup 2 + >r ( count counter up by 2, and store copy on return stack)
+		over >r ( store copy of pointadr back on return stack)
+		cells + ( set address to next relevant point)
+		dup 1 cells - ( we need this address 2 times, x- and y-coordinate)
+		@ swap @ ( the Top address was one cell less and therefore the x adress so after this line a new point is placed on the stack)
+		2swap >r >r ( take the two loop parameters and store them back on the return stack)
+
+	loop
+	r> r> drop drop ; ( take pointadr and counter from the return stack and drop them so that we get the same return stack at the end of the function as at the start)
 
 \ saves all points in "points"
 : readPoints ( x1 y1 x2 y2 ... xn yn pointsadr length -- pointsadr length)
 	2 * dup
 	0 u+do
 		\ push the length on the return stack, arrange the next coordinate in the correct way (value adr) then take the length from the return stack, substract the loop counter and add that to the address. In this way we save the point in the correct order (x1 y1 x2 y2) 
-		>r swap over r> i i . over >r - cells + ! r>
-	loop ;
+		>r swap over r> i over >r - 1 - cells + ! r>
+	loop 2 / ;
 
-0 Value fd-in
-     0 Value fd-out
-     : open-input ( addr u -- )  r/o open-file throw to fd-in ;
-     : open-output ( addr u -- )  w/o create-file throw to fd-out ;
+\ solved with locals		 
+\ : whichSide ( x1 y1 x2 y2 x y -- x1 y1 x2 y2 x y d )
+\	swap >r >r 2swap 2dup r> swap - r> rot - ;
 
 : test
 	10 0 u+do
-		i .
+		i 1+ i .
 	loop ;
 
-1 2 3 4 5 6 7 8 9 0 5
+0 0 0 3 1 4 1 2 3 5 6 4 3 1 7 0 8
+
+\ number of points is on top of the stack, save it in length
 create length , 
+
+\ create an array to save the points in (since it has 2 coordinates its length*2 in length)
 create points length @ 2 * cells allot 
-points length @ readPoints
+
+\ pointadr length is put on the stack and array is filled
+points length @ readPoints graham .s
